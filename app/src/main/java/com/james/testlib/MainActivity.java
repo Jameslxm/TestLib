b3366d4522e6a2c489ca91de8e417a7b79876b30
Jameslxm <lixinman@126.com>
2016-08-26 16:36:03 +0800

    Update MainActivity.java

--- app/src/main/java/com/james/testlib/MainActivity.java
+++ app/src/main/java/com/james/testlib/MainActivity.java
@@ -1,13 +1,1 @@
-package com.james.testlib;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+https://github.com/Jameslxm/testlib/new/master?readme=1
